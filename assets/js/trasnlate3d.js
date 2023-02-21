c016afdfe18c35df259ba538dc83d9f9790adef3
Sayyadi68 <taiyoh68@gmail.com>
2023-02-21 12:07:01 +0330

    Loading finished
    Loading and some bugs fixed
    some files replaced or removed

--- assets/js/trasnlate3d.js
+++ assets/js/trasnlate3d.js
@@ -1,32 +1,4 @@
 var MovetoRight = document.getElementById("transform");
-
-// var i = 0;
-
-// do {
-
-//   task(i);
-//   i++;
-
-// } while (i <= 26);
-
-
-// function task(i) {
-
-//     setInterval(function() {
-        
-        // MovetoRight.style.transform = "translatex("+ 450*i +"px)"; 
-        // MovetoRight.style.transition = "all 0.25s ease 0s;"; 
-
-//         if (i == 26) {
-//             console.log(i)
-           
-//         }    
-
-//     }, 1000 * i );
-    
-// }
-
-
 var x = 0;
 
 var interval = setInterval(function() {
@@ -39,3 +11,4 @@ var interval = setInterval(function() {
     }
 
 },5000)
+
